8cbc024eb6a57aaabe0a8250b7beaccc2157388f
michalmaj90 <michalmaj90@gmail.com>
2018-03-21 12:35:49 +0100

    Created models and first view

--- guests/models.py
+++ guests/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+FOOD = (
+    (1, 'Normalne'),
+    (2, 'Wegetariańskie')
+)
 # Create your models here.
+
+class WeddingCouple(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    description = models.CharField(max_length=300)
+    email = models.CharField(max_length=64)
+    phone = models.IntegerField()
+
+class WeddingGuest(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    phone = models.IntegerField()
+    food = models.IntegerField(choices=FOOD)
+
+class AccompanyingPerson(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    food = models.IntegerField(choices=FOOD)
+    companion = models.OneToOneField(WeddingGuest, on_delete=models.CASCADE, primary_key=True)
+
+class WeddingInfo(models.Model):
+    church_name = models.CharField(max_length=200)
+    church_address = models.CharField(max_length=200)
+    church_view = models.CharField(max_length=300, null=True)
+    premises_name = models.CharField(max_length=200)
+    premises_address = models.CharField(max_length=200)
+    premises_view = models.CharField(max_length=300, null=True)
--- guests/views.py
+++ guests/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from guests.models import WeddingCouple, WeddingGuest, AccompanyingPerson, WeddingInfo
 
 # Create your views here.
+
+class HelloView(View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'base.html', ctx)
